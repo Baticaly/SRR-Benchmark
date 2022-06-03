8d8075798e1065fe319c7ee9b57f55635805c8c1
Baticaly <ta6nbe@gmail.com>
2022-06-03 03:58:38 +0200

    Removed altering on homography matrix

--- main.cpp
+++ main.cpp
@@ -20,11 +20,25 @@ int main( int argc, char** argv )
 {   
     chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
-    string image1_path = samples::findFile("set3overlap/2.png");
-    string image2_path = samples::findFile("set3overlap/1.png");
+    string image1_path = samples::findFile("set3overlap/1.png");
+    string image2_path = samples::findFile("set3overlap/2.png");
 
     Mat query = imread(image2_path, IMREAD_COLOR);
     Mat train = imread(image1_path, IMREAD_COLOR);
+
+    double xRender = train.cols + (2 * query.cols) + 50;
+    double yRender = train.rows + (query.rows / 2);
+
+    // Calculate center matrix
+    double midX = (xRender - train.cols) / 2;
+    double midY = (yRender - train.rows) / 2;
+
+    Mat C = (Mat_<double>(2,3) << 1, 0, midX, 0, 1, midY);
+
+    // Push train to center
+    warpAffine(train, train, C, Size(xRender, yRender));
+    warpAffine(query, query, C, Size(xRender, yRender));
+
 
     std::vector<KeyPoint> kpsA, kpsB;
     Mat descriptorsA, descriptorsB;
@@ -47,22 +61,22 @@ int main( int argc, char** argv )
     drawKeypoints(query, kpsA, result, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
 
     vector<DMatch> matches;
-    BFmatcher->match ( descriptorsA, descriptorsB, matches );
+    BFmatcher->match (descriptorsA, descriptorsB, matches);
 
     double min_dist=10000, max_dist=0;
 
     for (int i = 0; i < descriptorsA.rows; i++){
         double dist = matches[i].distance;
-        if ( dist < min_dist ) min_dist = dist;
-        if ( dist > max_dist ) max_dist = dist;
+        if(dist < min_dist) min_dist = dist;
+        if(dist > max_dist) max_dist = dist;
     }
 
     printf("-- Max dist : %f \n", max_dist);
     printf("-- Min dist : %f \n", min_dist);
 
-    std:vector< DMatch > good_matches;
+    std:vector<DMatch> good_matches;
     for (int i=0; i<descriptorsA.rows; i++){
-        if (matches[i].distance <= max (2*min_dist, 30.0)){
+        if (matches[i].distance <= max(2*min_dist, 30.0)){
             good_matches.push_back(matches[i]);
         }
     }
@@ -70,9 +84,8 @@ int main( int argc, char** argv )
     Mat img_match;
     Mat img_goodmatch;
 
-    drawMatches(query, kpsA, train, kpsB, matches, img_match);
+    //drawMatches(query, kpsA, train, kpsB, matches, img_match);
     drawMatches(query, kpsA, train, kpsB, good_matches, img_goodmatch);
-    //imshow("Matches", img_goodmatch);
 
     std::vector<Point2f> queryVector;
     std::vector<Point2f> trainVector;
@@ -88,13 +101,9 @@ int main( int argc, char** argv )
    [1.008416200554807, 0.0001126243523051494, -630.2860380694477;
     0.003563377880238407, 1.009041866561184, -2.453838492536256;
     1.265658829422426e-05, 4.221225619312872e-07, 1]
-
-    [ 1 , 0 , x_offset]
-    [ 0 , 1 , y_offset]
-    [ 0 , 0 ,    1    ]
     */
 
-
+    /*
     // Calculate total offset
     double xOffset = H.at<double>(0,2);
     double yOffset = H.at<double>(1,2);
@@ -110,20 +119,15 @@ int main( int argc, char** argv )
 
     // Push train to center
     warpAffine(train, train, C, Size(xRender, yRender));
-
+    warpAffine(query, query, C, Size(xRender, yRender));
+    
     // Multiply H matrix with the same center matrix
-    H.at<double>(0,2) = H.at<double>(0,2) + midX;
-    H.at<double>(1,2) = H.at<double>(1,2) + midY;
-
+    //H.at<double>(0,2) = H.at<double>(0,2) + midX;
+    //H.at<double>(1,2) = H.at<double>(1,2) + midY;
+    */
 
     Mat final;
     warpPerspective(query, final, H, Size(xRender, yRender));
-
-    
-    imshow("final", final);
-    waitKey(0);
-
-    Mat product(final, cv::Rect(0, 0, final.cols, final.rows));
 
     // Interpolation
     int fR, fG, fB, tR, tG, tB;
@@ -132,7 +136,6 @@ int main( int argc, char** argv )
 
             Vec3b & finalValue = final.at<Vec3b>(y,x);
             Vec3b & trainValue = train.at<Vec3b>(y,x);
-            Vec3b & productValue = product.at<Vec3b>(y,x);
             
             fR = finalValue[0]; fG = finalValue[1]; fB = finalValue[2]; 
             tR = trainValue[0]; tG = trainValue[1]; tB = trainValue[2]; 
@@ -141,26 +144,24 @@ int main( int argc, char** argv )
             int trainSum = tR + tG + tB;
             
             if( finalSum != 0 && trainSum != 0 ){
-                productValue[0] = (fR + tR) / 2;
-                productValue[1] = (fG + tG) / 2;
-                productValue[2] = (fB + tB) / 2;
+                finalValue[0] = (fR + tR) / 2;
+                finalValue[1] = (fG + tG) / 2;
+                finalValue[2] = (fB + tB) / 2;
             }
             else{
-                productValue[0] = (fR + tR);
-                productValue[1] = (fG + tG);
-                productValue[2] = (fB + tB);
+                finalValue[0] = (fR + tR);
+                finalValue[1] = (fG + tG);
+                finalValue[2] = (fB + tB);
             }
 
-            //final.at<Vec3b>(Point(x,y)) = finalValue;
-            //train.at<Vec3b>(Point(x,y)) = trainValue;
-            product.at<Vec3b>(Point(x,y)) = productValue;
+            final.at<Vec3b>(Point(x,y)) = finalValue;
         }
     }
 
-    //train.copyTo(product);   # No interpolation, simple stacking
+    //train.copyTo(final);   # No interpolation, simple stacking
 
     //imshow("Good Matches & Object detection", img_goodmatch);
-    imshow("Product", product);
+    imshow("Final", final);
 
     int matchSize;
     matchSize = sizeof(img_match);
